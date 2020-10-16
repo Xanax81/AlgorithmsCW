2ca5e0a0f836ed483713adee4da8f48bfbf45f00
Marcin <marcinoleksiak2000@gmail.com>
2020-10-16 22:23:56 +0200

    replaced original data with the one edited by Meriem

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas  # add pandas and xlrd into interpreter
 
-data = pandas.read_excel('data.xlsx')
+data = pandas.read_excel('data.xlsx')  # receiving data from given excel file
 print(data)
