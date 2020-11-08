c646b2a1473cb64c78db7313cbca96e2e4d0d08a
Meriem Kaddouri <meriemkaddouri@gmail.com>
2020-11-08 22:17:12 +0000

    -GUI without table and without the bakerloo rush hour tickbox -didn't add the Dlr yet

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+
 import pandas  # add pandas and xlrd into interpreter
 
 data = pandas.read_excel('data.xlsx')  # receiving data from given excel file
@@ -18,14 +21,12 @@ for i in range(len(data)):
 
     if dataToStation[i][-1] == ' ':
         dataToStation[i] = dataToStation[i][:-1]
-
 betweenCertainHours = 1 # TODO: make tickbox in GUI and link it here
 
 if betweenCertainHours:
     for i in range(len(data)):
         if dataLine[i] == 'Bakerloo':
             dataTravelTime[i] /= 2
-
 
 dataActualTravelTime = dataTravelTime
 for i in range(len(data)):
@@ -62,31 +63,6 @@ for i in range(len(data)):
         stationLines[dataToStation[i]] = []
         stationLines[dataToStation[i]].append(dataLine[i])
 
-def input_starting_station():
-    startingStation = input("What is your starting station?")
-    if startingStation not in dataFromStation:
-        if startingStation not in dataToStation:
-            print("There is no such a station D:")
-            startingStation = input_starting_station()
-            return startingStation
-        else:
-            return startingStation
-    else:
-        return startingStation
-
-
-def input_destination():
-    destination = input("What is your destination?")
-    if destination not in dataToStation:
-        if destination not in dataFromStation:
-            print("There is no such a station D:")
-            destination = input_destination()
-            return destination
-        else:
-            return destination
-    else:
-        return destination
-
 
 def common_line(list1, list2):
     answer = False
@@ -97,7 +73,7 @@ def common_line(list1, list2):
     return answer
 
 
-def dijkstra(starting_station, destination):  # TODO: hard test
+def dijkstra(starting_station, destination):
     shortest_distance = {}
     track_predecessor = {}
     track_predecessor_line = {starting_station: stationLines[starting_station]}
@@ -145,15 +121,92 @@ def dijkstra(starting_station, destination):  # TODO: hard test
     track_path.insert(0, starting_station)
     track_lines.insert(0, stationLines[starting_station])
 
-    # TODO: make GUI displaying results color stations and stuff... I hate frontend D:
+
     if shortest_distance[destination] != 999999:
         print("Shortest journey time is: " + str(shortest_distance[destination] - 1) + " minutes")
         print("Optimal path is: " + str(track_path))
         print(track_lines)
 
 
-#  inputs TODO: put them in GUI
-startingStation = input_starting_station()
-destination = input_destination()
 
-dijkstra(startingStation, destination)
+
+class UndergroundGUI(tk.Tk):
+
+    def __init__(self, root):
+
+        self.root = root  # TK object
+        self.root.geometry('620x400')
+        Label_0 = ttk.Label(self.root, text='Welcome to Fantastic Route Planner', width="300", font=("Calibri", 30))
+        Label_0.pack(padx=(10, 0))
+
+        # User inputs
+        self.user_starting_point = tk.StringVar()
+        self.user_destination = tk.StringVar()
+
+        staring_label = ttk.Label(self.root, text='Starting station:')
+        staring_label.pack()
+        starting_entry = ttk.Entry(self.root, width=15, textvariable=self.user_starting_point)
+        starting_entry.pack()
+
+        destination_label = ttk.Label(self.root, text='Destination:')
+        destination_label.pack()
+        self.destination_entry = ttk.Entry(self.root, width=15, textvariable=self.user_destination)
+        self.destination_entry.pack()
+
+        Planning_button = ttk.Button(self.root, text='Start planning your journey', command=self.plan_journey_now)
+        Planning_button.pack()
+
+        quit_button = ttk.Button(self.root, text='Exit Fantastic route Planner', command=self.root.destroy)
+        quit_button.pack()
+
+    def plan_journey_now(self):
+
+        print("Planning Journey")
+        # getting the input values
+        start_station = self.user_starting_point.get()
+        destination = self.user_destination.get()
+
+        print(start_station, destination)
+
+        # calling dijkstra alg find the route
+        path = dijkstra(start_station, destination)
+        print(path)
+
+    def input_starting_station(self):
+
+        # TODO: consider if the user input is lower case, it's also a bit confusing, maybe recheck the logic and the code
+        startingStation = self.user_starting_point.get()
+        print(startingStation)
+        if startingStation not in dataFromStation:
+            if startingStation not in dataToStation:
+                print("There is no such a station D:")
+                startingStation = self.input_starting_station()
+                return startingStation
+            else:
+                return startingStation
+        else:
+            return startingStation
+
+    def input_destination(self):
+        destination = self.user_destination.get()
+        print(destination)
+        if destination not in dataToStation:
+            if destination not in dataFromStation:
+                print("There is no such a station D:")
+                destination = self.input_destination()
+                return destination
+            else:
+                return destination
+        else:
+            return destination
+
+
+def start_gui():
+
+    root = tk.Tk()
+    app = UndergroundGUI(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    start_gui()
